24cdc1becfc52af0308e8a37345c1e47965f5e38
Jonas Lekgau <77699400+codewithwest@users.noreply.github.com>
2023-06-07 08:12:53 +0200

    admn template

---
+++ admin/js/admin.js
@@ -0,0 +1,46 @@
+document.querySelector('.collapse-nav').addEventListener('click', (e) => {
+    e.preventDefault()
+    el = document.querySelector('.nav')
+    el.style.display != "none" ?
+        el.style.display = "none" :
+        el.style.display = "grid"
+})
+
+
+for (let prd = 0; prd < 5; prd++) {
+    document.querySelector('.content').innerHTML += `
+        <div class="product-cont d-flex wrap">
+            <div class="product-img   d-flex center-content">
+                <img class="m-auto radius-circle" src="./assets/image.png" />
+            </div>
+            <div class="product-desc  flex-col">
+            <h2 class="fill center-content"><b>Description <br/> </b></h2>
+                This does represent the product desc
+                This does represent the product desc
+                This does represent the product desc
+            </div>
+             <div class="product-price flex-col center-content">
+             <h2 class="fill center-content"><b>Price <br/> </b></h2>
+             
+                <p class="m-auto"> R 1083 </p>
+            </div>
+            <div class="product-avail flex-col">
+             <h2 class="fill center-content"><b> Availability <br/> </b></h2>
+               <p class="m-auto">In Stock<p>
+            </div>
+            <div class="product-functions-cont flex-col">
+             <h2 class="fill center-content"><b>Functions <br/></b></h2>
+
+            <div class="product-functions d-flex m-auto center-content">
+
+                <button class="product-del b-none bg-none d-flex center-content">
+                    del
+            </button>
+            <button class="product-edit b-none bg-none d-flex center-content">
+                    edit
+            </button>  
+            </div>
+            </div>
+        </div>`
+
+}
